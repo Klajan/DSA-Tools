94772aac3835780469e98891d162c10d131a9f52
Klajan <48261494+Klajan@users.noreply.github.com>
2022-10-27 00:10:00 +0200

    add typedefs for fernkampf

---
+++ src/app/fernkampf/types.ts
@@ -0,0 +1,18 @@
+enum WaffenTyp {
+    Bogen,
+    Armbrust,
+    Wurfwaffe,
+    Schleuder,
+    Blasrohr
+}
+
+enum Deckung {
+    keine = 0,
+    halbe = +2,
+    dreiviertel = +4
+}
+
+export {
+    WaffenTyp,
+    Deckung
+}
