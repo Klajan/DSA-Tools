bf8163ccf3f0b6760fc6aef7f80223ec5a1047b9
Klajan <48261494+Klajan@users.noreply.github.com>
2022-10-28 14:33:40 +0200

    update layout for value store

--- src/app/fernkampf/value-store-fern.service.ts
+++ src/app/fernkampf/value-store-fern.service.ts
@@ -7,7 +7,7 @@ import { WaffenTyp, Deckung } from './types'
 })
 export class ValueStoreFernService {
 
-  private _waffentyp: WaffenTyp = WaffenTyp.Wurfwaffe;
+  waffentyp: WaffenTyp = WaffenTyp.Wurfwaffe;
 
   private static readonly _defaults = {
     distanz: 1,
@@ -30,15 +30,21 @@ export class ValueStoreFernService {
   sicht: number = 0;
   bewegung: number = 2;
   ziel: number = 3;
+  // Deckung
+  hasDeckung: boolean = false;
   deckung: number = 0;
+  // Wind
+  isWind: boolean = false;
   wind: number = 0;
+  // Steilschuss/Wurf
+  isSteilschuss: boolean = false;
   steilschuss: number = 0;
   steilwurf: number = 0;
 
   // Reittier
   reittier: boolean = false;
   reitbewegung: number = 0;
-  ohneSattel: boolean = false;
+  reitOhneSattel: boolean = false;
   
 // Ansagen
   ansage: number = 0; //max TaW (limit 32?)
@@ -48,13 +54,26 @@ export class ValueStoreFernService {
 
 //<-----This should be saved and persistant----->
 // Vor- / Nachteile
-  nachtsicht: boolean = false;
-  daemmersicht: boolean = false;
+  private _nachtsicht: boolean = false;
+  set nachtsicht(value: boolean) {
+    this._nachtsicht = value;
+  }
+  get nachtsicht() {
+    return this._nachtsicht;
+  }
+
+  private _daemmersicht: boolean = false;
+  set daemmersicht(value: boolean) {
+    this._daemmersicht = value;
+  }
+  get daemmersicht() {
+    return this._nachtsicht ? true : this._daemmersicht;
+  }
+  nachtblind: boolean = false;
   entfernungsinn: boolean = false;
   einaeugig: boolean = false;
   farbenblind: boolean = false;
   kurzsichtig: boolean = false;
-  nachtblind: boolean = false;
 
 // Sonderfertigkeiten
   scharfschuetze: boolean = false;
@@ -69,4 +88,5 @@ export class ValueStoreFernService {
   zweiteAT: boolean = false;
   
   constructor() { }
+
 }
