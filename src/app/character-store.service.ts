35ee5f3d17ffff24edbe20396de9f50c10d53cee
Klajan <48261494+Klajan@users.noreply.github.com>
2022-11-04 14:18:10 +0100

    add character store keeps track of all character specific values

---
+++ src/app/character-store.service.ts
@@ -0,0 +1,209 @@
+import { Injectable } from '@angular/core';
+import { BaseValueStore } from './base-value-store';
+
+import { LichtVorteil, WaffentypFern, Scharfschütze } from './types/char-enums';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterStoreService extends BaseValueStore {
+
+  private _waffentypFern: WaffentypFern = WaffentypFern.Wurfwaffe;
+  public get waffentypFern(): WaffentypFern {
+    return this._waffentypFern;
+  }
+  public set waffentypFern(value: WaffentypFern) {
+    if (this._waffentypFern === value) return;
+    this._waffentypFern = value;
+    this.valuesChanged();
+  }
+
+  // Vor- / Nachteile
+  private _lichtVorteil: LichtVorteil = LichtVorteil.None;
+  public get lichtVorteil(): LichtVorteil {
+    return this._lichtVorteil;
+  }
+  public set lichtVorteil(value: LichtVorteil) {
+    if (this._lichtVorteil === value) return;
+    this._lichtVorteil = value;
+    this.valuesChanged();
+  }
+
+  get nachtsicht() {
+    return this._lichtVorteil === LichtVorteil.Nachtsicht;
+  }
+  set nachtsicht(value: boolean) {
+    if (this.nachtsicht === value) return;
+    this._lichtVorteil = value ? LichtVorteil.Nachtsicht : LichtVorteil.None;
+    this.valuesChanged();
+  }
+
+  get daemmersicht() {
+    return this._lichtVorteil === LichtVorteil.Dämmerungssicht;
+  }
+  set daemmersicht(value: boolean) {
+    if (this.daemmersicht === value) return;
+    this._lichtVorteil = value ? LichtVorteil.Dämmerungssicht : LichtVorteil.None;
+    this.valuesChanged();
+  }
+
+  public get nachtblind(): boolean {
+    return this._lichtVorteil === LichtVorteil.Nachtblind;
+  }
+  public set nachtblind(value: boolean) {
+    if (this.nachtblind === value) return;
+    this._lichtVorteil = value ? LichtVorteil.Nachtblind : LichtVorteil.None;
+    this.valuesChanged();
+  }
+
+  private _entfernungsinn: boolean = false;
+  public get entfernungsinn(): boolean {
+    return this._entfernungsinn;
+  }
+  public set entfernungsinn(value: boolean) {
+    if (this._entfernungsinn === value) return;
+    this._entfernungsinn = value;
+    this.valuesChanged();
+  }
+
+  private _einaeugig: boolean = false;
+  public get einaeugig(): boolean {
+    return this._einaeugig;
+  }
+  public set einaeugig(value: boolean) {
+    if (this._einaeugig === value) return;
+    this._einaeugig = value;
+    this.valuesChanged();
+  }
+
+  private _farbenblind: boolean = false;
+  public get farbenblind(): boolean {
+    return this._farbenblind;
+  }
+  public set farbenblind(value: boolean) {
+    if (this._farbenblind === value) return;
+    this._farbenblind = value;
+    this.valuesChanged();
+  }
+
+  private _kurzsichtig: boolean = false;
+  public get kurzsichtig(): boolean {
+    return this._kurzsichtig;
+  }
+  public set kurzsichtig(value: boolean) {
+    if (this._kurzsichtig === value) return;
+    this._kurzsichtig = value;
+    this.valuesChanged();
+  }
+
+  // Sonderfertigkeiten
+  private _scharfschütze: Scharfschütze = Scharfschütze.None;
+  public get scharfschuetzeEnum(): Scharfschütze {
+    return this._scharfschütze;
+  }
+  public set scharfschuetzeEnum(value: Scharfschütze) {
+    this._scharfschütze = value;
+  }
+
+  public get scharfschuetze(): boolean {
+    return this._scharfschütze === Scharfschütze.Scharfschütze;
+  }
+  public set scharfschuetze(value: boolean) {
+    if (this.scharfschuetze === value) return;
+    this._scharfschütze = value ? Scharfschütze.Scharfschütze : Scharfschütze.None;
+    this.valuesChanged();
+  }
+
+  public get meisterschuetze(): boolean {
+    return this._scharfschütze === Scharfschütze.Meisterschütze;
+  }
+  public set meisterschuetze(value: boolean) {
+    if (this.meisterschuetze === value) return;
+    this._scharfschütze = value ? Scharfschütze.Meisterschütze : Scharfschütze.None;
+    this.valuesChanged();
+  }
+
+  private _schnellladen: boolean = false;
+  public get schnellladen(): boolean {
+    return this._schnellladen;
+  }
+  public set schnellladen(value: boolean) {
+    if (this._schnellladen === value) return;
+    this._schnellladen = value;
+    this.valuesChanged();
+  }
+
+  private _schnellziehen: boolean = false;
+  public get schnellziehen(): boolean {
+    return this._schnellziehen;
+  }
+  public set schnellziehen(value: boolean) {
+    if (this._schnellziehen === value) return;
+    this._schnellziehen = value;
+    this.valuesChanged();
+  }
+
+  private _eisenhagel: boolean = false;
+  public get eisenhagel(): boolean {
+    return this._eisenhagel;
+  }
+  public set eisenhagel(value: boolean) {
+    if (this.eisenhagel === value) return;
+    this._eisenhagel = value;
+    this.valuesChanged();
+  }
+
+  private _berittenerschuetze: boolean = false;
+  public get berittenerschuetze(): boolean {
+    return this._berittenerschuetze;
+  }
+  public set berittenerschuetze(value: boolean) {
+    if (this._berittenerschuetze === value) return;
+    this._berittenerschuetze = value;
+    this.valuesChanged();
+  }
+
+  constructor(persistentStore: LocalStorageService) {
+    super();
+    const KEY = "CHARACTER_V0"
+    persistentStore.register(KEY, this.getPersistenceObject.bind(this))
+      .then((value) => {
+        this.setFromPersistence(value as ReturnType<CharacterStoreService['getPersistenceObject']>);
+      });
+    console.debug(this.getPersistenceObject());
+  }
+
+  private getPersistenceObject() {
+    if (!this.hasChanged) return null;
+    this.hasChanged = false;
+    return {
+      waffentypFern: this.waffentypFern,
+      lichtVorteil: this.lichtVorteil,
+      entfernungsinn: this.entfernungsinn,
+      einaeugig: this.einaeugig,
+      farbenblind: this.farbenblind,
+      kurzsichtig: this.kurzsichtig,
+      scharfschuetzeEnum: this.scharfschuetzeEnum,
+      schnellladen: this.schnellladen,
+      schnellziehen: this.schnellziehen,
+      eisenhagel: this.eisenhagel,
+      berittenerschuetze: this.berittenerschuetze,
+    }
+  }
+
+  private setFromPersistence(object: ReturnType<CharacterStoreService['getPersistenceObject']> | null) {
+    if (object === null) return;
+    this.waffentypFern = object.waffentypFern;
+    this.lichtVorteil = object.lichtVorteil;
+    this.entfernungsinn = object.entfernungsinn;
+    this.einaeugig = object.entfernungsinn;
+    this.farbenblind = object.farbenblind;
+    this.kurzsichtig = object.kurzsichtig;
+    this.scharfschuetzeEnum = object.scharfschuetzeEnum;
+    this.schnellladen = object.schnellladen;
+    this.schnellziehen = object.schnellziehen;
+    this.eisenhagel = object.eisenhagel;
+    this.berittenerschuetze = object.berittenerschuetze;
+  }
+}
